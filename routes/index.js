7658121ac2666ba56694cce0dc903729e33bed7f
pedrotroncoso28 <PEDRO.TRONCOSOJIMENEZ02@login.cuny.edu>
2025-12-03 21:08:36 -0500

    Read data from my database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+module.exports = router;
+
 var express = require('express');
 var router = express.Router();
 
+// Import my Song model
+const Song = require('../models/songs'); 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', list: [] });
+router.get('/', async function(req, res, next) {
+  try {
+    // Fetch all songs from MongoDB
+    const songs = await Song.find();
+
+    // Render index view and pass songs data
+    res.render('index', { 
+      title: 'My Song List',
+      songsList: songs 
+    });
+
+  } catch (err) {
+    console.error('Error fetching songs:', err);
+    res.status(500).send('Error fetching songs');
+  }
 });
 
 module.exports = router;
