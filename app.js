d909ae7f2a85655c96942c6a8c47f9e5108d2244
pedrotroncoso28 <PEDRO.TRONCOSOJIMENEZ02@login.cuny.edu>
2025-12-10 23:39:01 -0500

    Adding post, edit, and delete

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var methodOverride = require('method-override');
-
+var logger = require('morgan'); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -31,8 +29,7 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method')); 
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -46,13 +43,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+res.locals.message = err.message;
+res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+res.status(err.status || 500);
+res.render('error');
 });
 
-module.exports = app
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
-
-
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const Song = require("../models/songs");
 
 /* GET home page */
-router.get('/', async (req, res) => {
+router.get("/", async function (req, res, next) {
   const songs = await Song.find();
-  res.render('index', { 
-    title: 'Christmas Song List',
-    songsList: songs
-  });
+  res.render("index", { title: "Christmas List", songsList: songs });
 });
 
-/* POST: add new song */
+/* POST add new song */
 router.post("/add", async (req, res) => {
-  const newTitle = req.body.title;
+  await Song.create({ title: req.body.title });
+  res.redirect("/");
+});
 
-  await Song.create({ title: newTitle });
+/* DELETE a song */
+router.post("/delete/:id", async (req, res) => {
+  await Song.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
 
+/* GET edit page */
+router.get("/edit/:id", async (req, res) => {
+  const song = await Song.findById(req.params.id);
+  res.render("edit", { song });
+});
+
+/* POST save edited song */
+router.post("/edit/:id", async (req, res) => {
+  await Song.findByIdAndUpdate(req.params.id, { title: req.body.title });
   res.redirect("/");
 });
 
